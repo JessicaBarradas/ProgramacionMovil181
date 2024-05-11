e93bd7f5b5a3f8ac95bda3fa940c5b63da69ba27
JessicaBarradas <121040932@upq.edu.mx>
2024-05-11 15:05:21 -0600

    Comentarios

---
+++ 01-Tipos/05-Comentarios.js
@@ -0,0 +1,14 @@
+// ahuhsbudhc
+/**
+ * Este es un comentario
+ * de muchas
+ * lineas
+ */
+/*
+Todo lo que escribamos entremedio
+de los slash y los asteriscos
+va a ser ignorado
+*/
+let numero = 42; //Este es numero (nunca hacer porque ya estamos especificando)
+
+console.log(numero);
