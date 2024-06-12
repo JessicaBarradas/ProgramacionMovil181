085f2142197a125f4610560324de48af11ae0c62
JessicaBarradas <125698322+JessicaBarradas@users.noreply.github.com>
2024-06-11 19:29:45 -0600

    Add files via upload

---
+++ index.js
@@ -0,0 +1,23 @@
+console.log('Hola consola')
+/*
+//Funciones
+//Tradicional
+function suma(a,b){
+    return a+b
+}
+//Fat Arrow Function
+const resta=(a,b)=>{return a-b}
+//Fat Arrow simplificada
+const resta1=(a,b)=>a-b
+
+console.log(suma(1,10))
+console.log(resta(100,10))
+console.log(resta1(1000,10))*/
+
+//OBJETOS
+const usuario={
+    nombre:'Jessica',
+    apellido:'Breton',
+    edad:30,
+    direccion:''
+}
